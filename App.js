41f19895a453f1d26ef6fd034323880d52b5a92e
dimazhylko <zhylko.dima@gmail.com>
2018-10-28 12:35:26 +0100

    Details screen added

--- App.js
+++ App.js
@@ -1,39 +1,76 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, FlatList, SafeAreaView, StatusBar} from 'react-native';
+import {Platform, StyleSheet, Text, View, FlatList, SafeAreaView, StatusBar, TouchableHighlight} from 'react-native';
+import {createStackNavigator} from 'react-navigation'
 
-export default class App extends Component {
 
-    constructor(props){
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'List of eateries',
+    };
+    constructor(props) {
         super(props);
         this.state = {isLoading: true};
         this.getEateries()
     }
 
-    getEateries(){
-        fetch("http://localhost:9000/allEateries").then((response)=>response.json()).then((responseJson)=>{
+    getEateries() {
+        fetch("http://localhost:9000/allEateries").then((response) => response.json()).then((responseJson) => {
             this.setState({
-               isLoading: false,
-               dataSource: responseJson
+                isLoading: false,
+                dataSource: responseJson
             });
         })
     }
 
     render() {
         return (
-            <View style={{ marginTop: StatusBar.currentHeight }}>
-            <SafeAreaView>
-                <FlatList
-                    data= {this.state.isLoading ? []:this.state.dataSource}
-                    renderItem={({item}) => <View style={ styles.listItem }>
-                        <Text style={ styles.listItemNameText }>{item.name}</Text>
-                    </View>}
-                />
-            </SafeAreaView>
+            <View style={{marginTop: StatusBar.currentHeight}}>
+                <SafeAreaView>
+                    <FlatList
+                        data={this.state.isLoading ? [] : this.state.dataSource}
+                        renderItem={({item}) => <TouchableHighlight onPress={()=>this.props.navigation.navigate('Details', {item: item})}><View style={styles.listItem}>
+                            <Text style={styles.listItemNameText}>{item.name}</Text>
+                        </View></TouchableHighlight>}
+                    />
+                </SafeAreaView>
             </View>
         );
     }
 }
 
+class DetailsScreen extends React.Component{
+
+    static navigationOptions = {
+        title: 'Details',
+    };
+
+
+
+    render() {
+
+        const passedData = this.props.navigation.getParam('item', {name: 'Data corrupted, sorry', x: 1, y:1});
+
+        return (
+            <View style={{marginTop: StatusBar.currentHeight}}>
+                <SafeAreaView>
+                    <Text>{passedData.name}</Text>
+                    <Text>x = {passedData.x}</Text>
+                    <Text>y = {passedData.y}</Text>
+                </SafeAreaView>
+            </View>
+        )
+    }
+}
+
+
+const RootStack = createStackNavigator({
+    Home: HomeScreen,
+    Details: DetailsScreen
+}, {
+    initialRouteName: 'Home',
+});
+
+export default RootStack;
 
 
 const styles = StyleSheet.create({
