aa4b4a8d23fa43d2cc7ef6f945e952c89cceb0b4
dimazhylko <zhylko.dima@gmail.com>
2018-11-12 19:32:46 +0100

    Remove unnecessary functionality

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -4,33 +4,17 @@ import MapView from 'react-native-maps'
 
 const { width, height } = Dimensions.get('window')
 const ASPECT_RATIO = width / height
-const LATITUDE_DELTA = 0.0922
+const LATITUDE_DELTA = 0.0122
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
 
-const EDGE_PADDING = {
-  top: 100,
-  right: 100,
-  bottom: 100,
-  left: 100
-}
-
 export class DetailsScreen extends React.Component {
-
   constructor (props) {
     super(props)
-    this.map = null
     this.state = {
       date: new Date(),
-      isLoading: true,
-      isDevicePositionDefined: false
+      isLoading: true
     }
-  }
-
-  animateToRegion = () => {
-    this.map.fitToCoordinates([this.state.devicePosition, this.state.eateryPosition], {
-      edgePadding: EDGE_PADDING,
-      animated: true
-    })
+    this.getEateryPosition(this.props.navigation.state.params.item.name)
   }
 
   getEateryPosition (name) {
@@ -51,27 +35,6 @@ export class DetailsScreen extends React.Component {
     })
   }
 
-  componentDidMount () {
-    this.getEateryPosition(this.props.navigation.state.params.item.name)
-    navigator.geolocation.getCurrentPosition((position) => {
-        this.setState({
-          devicePosition: position,
-          isDevicePositionDefined: true,
-        })
-      }, (error) => alert(JSON.stringify(error)),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 })
-  }
-
-  componentDidUpdate () {
-    if (this.state.isLoading === false && this.state.isDevicePositionDefined === true) {
-      this.animateToRegion()
-    }
-  }
-
-  static navigationOptions = ({ navigation }) => ({
-    title: navigation.state.params.item.name
-  })
-
   render () {
     const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
     const item = this.props.navigation.state.params.item
@@ -86,12 +49,12 @@ export class DetailsScreen extends React.Component {
         </View>
 
         <View style={{ flex: 2 }}>
-          {!this.state.isLoading ? <MapView style={styles.map}
-                                            initialRegion={this.state.region}
-                                            showsUserLocation={true}
-                                            ref={(map) => this.map = map}>
-            <MapView.Marker coordinate={this.state.eateryPosition}/>
-          </MapView> : <View><ActivityIndicator size={'small'} color={'rgba(0, 55, 167, 0.6)'}/></View>}
+          {!this.state.isLoading
+            ? <MapView style={styles.map}
+              initialRegion={this.state.region}
+              showsUserLocation={true}>
+              <MapView.Marker coordinate={this.state.eateryPosition}/>
+            </MapView> : <View><ActivityIndicator size={'small'} color={'rgba(0, 55, 167, 0.6)'}/></View>}
         </View>
       </View>
     )
